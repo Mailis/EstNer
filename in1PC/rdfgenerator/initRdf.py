e5b0341cccfdbfd9242166ab7b420180ff00d178
Mailis <maiilu@hotmail.com>
2015-07-29 00:01:27 +0300

    Update initRdf.py
    using NERD namespace

--- in1PC/rdfgenerator/initRdf.py
+++ in1PC/rdfgenerator/initRdf.py
@@ -27,43 +27,40 @@ class OntologyData():
     perName = FOAF["name"]
     givenName = FOAF["givenName"]
     familyName = FOAF["familyName"]
+    #an existing ontology for extracted named entities
+    nerd = Namespace("http://nerd.eurecom.fr/ontology#")
     #description about new (this) ontology:
-    ner = Namespace("http://www.estnernet.ee/nerdatanet#")
+    ner = Namespace("http://www.estner.ee/estnerd#")
     #construct namespace strings for objects' namespaces
-    locStr = "http://www.estnernet.ee/nerdatanet/locations#"
-    orgStr = "http://www.estnernet.ee/nerdatanet/organizations#"
-    perStr = "http://www.estnernet.ee/nerdatanet/people#"
+    locStr = "http://nerd.eurecom.fr/ontology#"
+    orgStr = "http://nerd.eurecom.fr/ontology#"
+    perStr = "http://nerd.eurecom.fr/ontology#"
     #description 'about resource':
-    locations = Namespace(locStr)
-    location = locations.Location
-    organizations = Namespace(orgStr)
-    organization = organizations.Organization
-    people = Namespace(perStr)
-    person = people.Person
+    location = nerd.Location
+    organization = nerd.Organization
+    person = nerd.Person
     
     
     #description of datatype property for lemmas:
-    lemma = ner.lemma#==URIRef("http://www.estnernet.ee/nerdatanet#lemma")
+    lemma = ner.lemma#==URIRef("http://www.estner.ee/estnerd#lemma")
     #description of datatype property mentionedAtSite:
-    mentionedAtSite = ner.mentionedAtSite#==URIRef("http://www.estnernet.ee/nerdatanet#mentionedAtSite")
+    mentionedAtSite = ner.mentionedAtSite#==URIRef("http://www.estner.ee/estnerd#mentionedAtSite")
     #description of datatype property orgName:
-    orgName = ner.orgName#==URIRef("http://www.estnernet.ee/nerdatanet#orgName")
+    orgName = ner.orgName#==URIRef("http://www.estner.ee/estnerd#orgName")
     #description of datatype property locationNames:
-    locationName = ner.locationName#==URIRef("http://www.estnernet.ee/nerdatanet#locationName")
+    locationName = ner.locationName#==URIRef("http://www.estner.ee/estnerd#locationName")
     
     LOC_TYPE = "LOC"
     ORG_TYPE = "ORG"
     PER_TYPE = "PER"
-    locRdf = ""
-    orgRdf = ""
-    perRdf = ""
+   
     m = Manager()
     #chunk lists
     sharedList_per = m.list()
     sharedList_org = m.list()
     sharedList_loc = m.list()
     
-    def __init__(self, namespaceURI = 'http://www.estnernet.ee/nerdatanet'):
+    def __init__(self, namespaceURI = 'http://nerd.eurecom.fr/ontology'):
         self.locRdf = comm.pathToRDFdir + self.LOC_TYPE + ".rdf"
         self.orgRdf = comm.pathToRDFdir + self.ORG_TYPE + ".rdf"
         self.perRdf = comm.pathToRDFdir + self.PER_TYPE + ".rdf"
@@ -82,12 +79,10 @@ class OntologyData():
     def getPerRdfGraph(self):
         g = Graph()
         return g.parse(self.perRdf)
-    
     def bindNamespaces(self, g):
-        g.bind("ner", OntologyData.ner)
-        g.bind("people", OntologyData.people)
-        g.bind("organizations", OntologyData.organizations)
-        g.bind("locations", OntologyData.locations)
+        g.bind("nerd", self.nerd)
+        g.bind("ner", self.ner)
+        g.bind("rdf", RDF)
         g.bind("owl", OWL)
         g.bind("dc", DC)
         g.bind("foaf", FOAF)
@@ -177,7 +172,6 @@ class OrganizationManager (OntologyData):
                             newOrg = URIRef(self.orgStr + objName.replace (">", "").replace ("<", "").replace ("|", "").replace (" ", "_").lower())
                             newOrgName = Literal(objName)
                             newWebpage = URIRef(webpage);
-            
                             #add triples
                             #check if graph contains bob already
                             if ( newOrg, RDF.type, URIRef(self.organization)) not in g:
@@ -199,7 +193,6 @@ class OrganizationManager (OntologyData):
                         except:
                             comm.printException(comm.initRdfErrorsFilePath, "build_org_graph")
                             pass
-            #print(str(newDataExists)) 
             #write rdf into file
             if (newDataExists):
                 try:
@@ -241,9 +234,9 @@ class PeopleManager (OntologyData):
                         
                         #add triples
                         #check if graph contains bob already
-                        if ( newPerson, RDF.type, FOAF.Person) not in g:
+                        if ( newPerson, RDF.type, URIRef(self.person)) not in g:
                             newDataExists = True
-                            g_new.add( (newPerson, RDF.type, FOAF.Person) )
+                            g_new.add( (newPerson, RDF.type, URIRef(self.person)) )
                             if(newGivenName != Literal("")):
                                 g_new.add( (newPerson, self.givenName, newGivenName) )
                             if(newFamilyName != Literal("")):
@@ -290,7 +283,7 @@ class RdfFilesCreator(OntologyData):
        This class is used only at the start, when no rdf files are created yet.
     '''
     
-    def __init__(self, od, namespaceURI = 'http://www.estnernet.ee/nerdatanet'):
+    def __init__(self, od, namespaceURI = 'http://nerd.eurecom.fr/ontology'):
         OntologyData.__init__(self)
         self.initGraphs(od, namespaceURI)
                
@@ -298,23 +291,18 @@ class RdfFilesCreator(OntologyData):
     def initGraphs(self, od, namespaceURI):
         if not (os.path.isfile(od.locRdf) | os.path.isfile(od.orgRdf) | os.path.isfile(od.perRdf)):
            
-            OntologyData.ner = Namespace(namespaceURI + "#")
+            OntologyData.nerd = Namespace(namespaceURI + "#")
+            OntologyData.ner = Namespace("http://www.estner.ee/estnerd#")
             
             #construct namespace strings for objects' namespaces
-            OntologyData.locStr = namespaceURI + "/locations#"
-            OntologyData.orgStr = namespaceURI + "/organizations#"
-            OntologyData.perStr = namespaceURI + "/people#"
-            OntologyData.locations = Namespace(self.locStr)
-            OntologyData.location = OntologyData.locations.Location
-            OntologyData.organizations = Namespace(self.orgStr)
-            OntologyData.organization = OntologyData.organizations.Organization
-            OntologyData.people = Namespace(self.perStr)
-            OntologyData.person = OntologyData.people.Person
+            OntologyData.locStr = OntologyData.nerd
+            OntologyData.orgStr = OntologyData.nerd
+            OntologyData.perStr = OntologyData.nerd
             
-            OntologyData.lemma = OntologyData.ner.lemma#==URIRef("http://www.estnernet.ee/nerdatanet#lemma")
-            OntologyData.mentionedAtSite = OntologyData.ner.mentionedAtSite#==URIRef("http://www.estnernet.ee/nerdatanet#mentionedAtSite")
-            OntologyData.orgName = OntologyData.ner.orgName#==URIRef("http://www.estnernet.ee/nerdatanet#orgName")
-            OntologyData.locationName = OntologyData.ner.locationName#==URIRef("http://www.estnernet.ee/nerdatanet#orgName")
+            OntologyData.lemma = OntologyData.ner.lemma#==URIRef("http://www.estner.ee/estnerd#lemma")
+            OntologyData.mentionedAtSite = OntologyData.ner.mentionedAtSite#==URIRef("http://www.estner.ee/estnerd#mentionedAtSite")
+            OntologyData.orgName = OntologyData.ner.orgName#==URIRef("http://www.estner.ee/estnerd#orgName")
+            OntologyData.locationName = OntologyData.ner.locationName#==URIRef("http://www.estner.ee/estnerd#orgName")
     
             g = Graph()
             #define specific namespace prefix
@@ -322,21 +310,12 @@ class RdfFilesCreator(OntologyData):
             
             #describe ner as ontology
             g.add( (URIRef(OntologyData.ner), RDF.type, OntologyData.owlOntology) )
-            g.add( (URIRef(OntologyData.ner), DC["title"], Literal("estNER Ontology")) )
-            g.add( (URIRef(OntologyData.ner), DC["description"], Literal("Extracted estNER locations, organizations and persons.")) )
-            #describe organization as class
-            g.add( (URIRef(OntologyData.organization), RDF.type, OntologyData.owlClass) )
-            g.add( (URIRef(OntologyData.organization), RDFS["label"], Literal("organization class")) )
-            g.add( (URIRef(OntologyData.organization), RDFS["comment"], Literal("Class for organizations extracted from web pages' contents by estNer.")) )
-            #describe location as class
-            g.add( (URIRef(OntologyData.location), RDF.type, OntologyData.owlClass) )
-            g.add( (URIRef(OntologyData.location), RDFS["label"], Literal("location class")) )
-            g.add( (URIRef(OntologyData.location), RDFS["comment"], Literal("Class for locations extracted from web pages' contents by estNer.")) )
+            g.add( (URIRef(OntologyData.ner), DC["title"], Literal("estNERD Ontology")) )
+            g.add( (URIRef(OntologyData.ner), DC["description"], Literal("Locations, organizations and persons extracted from Estonian open data, using 'Estnltk — Open source tools for Estonian natural language processing' [http://tpetmanson.github.io/estnltk/].")) )
             #define owl#DatatypeProperty
             g.add( (OntologyData.lemma, RDF.type, OntologyData.datatypeProperty) )
             g.add( (OntologyData.mentionedAtSite, RDF.type, OntologyData.datatypeProperty) )
-            g.add( (OntologyData.orgName, RDF.type, OntologyData.datatypeProperty) )
-            g.add( (OntologyData.locationName, RDF.type, OntologyData.datatypeProperty) )
+
             
             self.locationGraph = g
             self.organizatioGraph = g
@@ -350,4 +329,3 @@ class RdfFilesCreator(OntologyData):
             self.locationGraph.serialize(od.perRdf, format='pretty-xml', encoding='utf-8')
         
       
-
